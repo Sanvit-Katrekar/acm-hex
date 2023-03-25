3d01a09f000b480ad1f18eba5939dd2fff1fa532
sanvit <katrekarsanvit2@gmail.com>
2023-03-25 13:09:47 +0400

    fixed the pwd script

--- .secret/gen_master_pwd.py
+++ .secret/gen_master_pwd.py
@@ -1,6 +1,12 @@
 password1 = input("Enter first password: ")
 password2 = input("Enter second password: ")
 
+try:
+    if password1[0] == '4' and password2[0] == 'n':
+        password1, password2 = password2, password1
+except IndexError:
+    print("Passwords should atleast be of length 1!")
+
 master_password = password1[::-1] + "_M-M_" + password2[::-1]
 
 print(master_password)
